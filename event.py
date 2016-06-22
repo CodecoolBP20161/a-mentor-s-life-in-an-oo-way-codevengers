2ddd826297f7afa31ff37b49ee1476c4a615b6f9
oliver <kiss.oliver.attila@gmail.com>
2016-06-22 10:27:43 +0200

    updated event class after specification

---
+++ event.py
@@ -0,0 +1,8 @@
+
+class Event:
+    def __init__(self, name, time, place, length, attendants):
+        self.name = name
+        self.time = time
+        self.place = place
+        self.length = length
+        self.attendants = attendants
