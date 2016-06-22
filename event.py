9420d1516eff127f13bb3902de9182ae07756655
oliver <kiss.oliver.attila@gmail.com>
2016-06-22 11:15:17 +0200

    PrivateMentoring class implemented

--- event.py
+++ event.py
@@ -1,6 +1,8 @@
+from mentor import Mentor
+from student import Student
 
 class Event:
-    def __init__(self, name, time, place, length, attendants):
+    def __init__(self, name, time, place, length, attendants=[]):
         self.name = name
         self.time = time
         self.place = place
---
+++ private_mentoring.py
@@ -0,0 +1,20 @@
+from event import Event
+from student import Student
+from mentor import Mentor
+
+class PrivateMentoring(Event):
+    def __init__(self, students, mentor, *args, *kwargs):
+        super().__init__(*args, *kwargs)
+        self.students = students
+        self.mentor = mentor
+
+    def do_event(self):
+        for student in self.students:
+            student.happy = True
+            student.knowledge_level += 10
+            student.motivation_level += 10
+            student.energy_level -= 5
+
+        self.mentor.happy = True
+        self.mentor.motivation_level += 150
+        self.mentor.energy_level -= 5
