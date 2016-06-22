6a855420903f7f10a1ec0c69a46c1e08d31de933
Dávid Fehér <feherkeee@gmail.com>
2016-06-22 11:27:39 +0200

    Implemented Mentor class, needs testing

--- mentor.py
+++ mentor.py
@@ -1,5 +1,61 @@
+import random
 from person import Person
+from codecool_class import CodecoolClass
+from student import Student
+from candidate import Candidates
 
 
 class Mentor(Person):
-    pass
+
+    @classmethod
+    def create_by_csv(cls, file_name):
+        with open(file_name) as csvfile:
+            mentors = csv.reader(csvfile,  delimiter=';')
+            mentors_list = []
+            for row in mentors:
+                mentors_list.append(row)
+        return mentors_list
+
+    def __init__(self, first_name,  last_name, year_of_birth, gender,  nickname):
+        super().__init__(first_name,
+                         last_name,
+                         year_of_birth,
+                         gender)
+
+        self.nickname = nickname
+        self.energy_level = None
+
+    def do_morning_gym(students, mentors):
+        for student in students:
+            if student.energy_level < 10:
+                student.energy_level += 20
+            else:
+                student.energy_level += 10
+        for mentor in mentors:
+            if mentor.energy_level < 10:
+                mentor.energy_level += 20
+            else:
+                mentor.energy_level += 10
+        return students, mentors
+
+    def daily_agenda(students):
+        for student in students:
+            if studied_level:
+                students.motivation_level += 10
+            else:
+                students.motivation_level -= 10
+
+    def mentoring(student):
+        student.happy = True
+        student.knowledge_level += 10
+        student.motivation_level += 10
+        student.energy_level -= 10
+
+    def interview(candidate, mentors):
+        candidate_in_school = True
+        choice = 0
+        for mentor in mentors:
+            choice += random.randrange(2)
+        if choice == 3:
+            accepted = True
+            candidate.motivation_level += 10
