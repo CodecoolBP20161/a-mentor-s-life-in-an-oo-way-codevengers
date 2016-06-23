060ccb9f9f4108a7d6e1a16c441c7f423d97b218
Dávid Fehér <feherkeee@gmail.com>
2016-06-23 09:58:40 +0200

    event.py default arguments fixed

--- event.py
+++ event.py
@@ -2,9 +2,13 @@ from student import Student
 
 
 class Event:
-    def __init__(self, name, time, place, length, attendants=[]):
+    def __init__(self, name, time, place, length, attendants=None):
+        if attendants is None:
+            self.attendants = []
+        else:
+            self.attendants = attendants
         self.name = name
         self.time = time
         self.place = place
         self.length = length
-        self.attendants = attendants
+        
